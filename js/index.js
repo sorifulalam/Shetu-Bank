3a57282d5ea7bd543afb7649615f87231af2e69a
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-15 00:51:34 +0600

    js add

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById('login')
+    loginBtn.addEventListener('click',function(){
+       const loginArea = document.getElementById('log-in-area')
+       loginArea.style.display="none"
+    })
