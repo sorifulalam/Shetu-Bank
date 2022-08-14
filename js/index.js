85ed012263fcb54bfd1ac4e87ef6b52e4b359565
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-15 04:13:23 +0600

    update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const loginBtn = document.getElementById('login')
     loginBtn.addEventListener('click',function(){
        const loginArea = document.getElementById('log-in-area')
        loginArea.style.display="none";
-        const transactionArea = document.getElementById('transation-area')
-        transactionArea.style.display="block" 
+        const transactionArea = document.getElementById('transaction-area') 
+        transactionArea.style.display = 'block'
     })
