1c7d4129625116556a6bd038c51b940e6bc59927
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-16 20:00:05 +0600

    upp

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+//login button event handelar
 const loginBtn = document.getElementById('login')
     loginBtn.addEventListener('click',function(){
        const loginArea = document.getElementById('log-in-area')
        loginArea.style.display="none";
         const transactionArea = document.getElementById('transaction-area') 
         transactionArea.style.display = 'block'
+    })
+
+
+ //deposit busston event handelar
+ 
+    const depositBtn = document.getElementById('adddeposit')
+    depositBtn.addEventListener('click',function(){
+        const despositAmount = document.getElementById('depositamount').value
+        const depositNumber = parseFloat(despositAmount)
     })
