8aec3741c77eb45a1e912bf522e1ec86ee71ca26
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2022-08-19 16:56:08 +0600

    uppdate all

--- js/index.js
+++ js/index.js
@@ -6,38 +6,39 @@ const loginBtn = document.getElementById('login')
         const transactionArea = document.getElementById('transaction-area') 
         transactionArea.style.display = 'block'
     })
-
-
+    
+    
  //deposit busston event handelar
  
     const depositBtn = document.getElementById('adddeposit')
     depositBtn.addEventListener('click',function(){
         // console.log('shetu')
-        const despositAmount = document.getElementById('depositamount').value
+        const despositAmount = document.getElementById('depositAmount').value
         const depositNumber = parseFloat(despositAmount)
-    //    // console.log(depositNumber)
+       // console.log(depositNumber)
     //    const curentDeposit = document.getElementById('curentDeposit').innerText
     //    const curenDepositNumber = parseFloat(curentDeposit)
     //    //console.log()
     //    const totalDeposit = depositNumber + curenDepositNumber;
     //    document.getElementById('curentDeposit').innerText =totalDeposit
        
+    //   const curentBalance = document.getElementById('curenBalance').innerText
+    // const curentBalanceNumber = parseFloat(curentBalance)
+    // const curentBalanceTotal = depositNumber + curentBalanceNumber
+    // document.getElementById('curenBalance').innerText = curentBalanceTotal
 
-        // const curentBalance = document.getElementById('curenBalance').innerText
-        // const curentBalanceNumber = parseFloat(curentBalance)
-        // const curentBalanceTotal = depositNumber + curentBalanceNumber
-        // document.getElementById('curenBalance').innerText = curentBalanceTotal
 
-        document.getElementById('depositamount').value=""
+        document.getElementById('depositAmount').value=""
 
-        spanUpdatetext = ('curentDeposit' ,depositNumber)
-        spanUpdatetext = ('curenBalance' ,depositNumber)
+       
+        spanUpdateText('curentDeposit',depositNumber)
+        spanUpdateText('curenBalance' ,depositNumber);
       
     })
 
-   function spanUpdatetext(id , depositNumber){
-        const curent =document.getElementById(id).innerText
-        const curentNumber = parseFloat(curent)
-        const curentTotal = depositNumber + curentNumber
-        document.getElementById(id).innerText = curentTotal
-   }
+    function spanUpdateText(id, depositNumber){
+        const current= document.getElementById(id).innerText;
+        const currentNumber=parseFloat(current);
+        const totalAmount=currentNumber+depositNumber;
+        document.getElementById(id).innerText=totalAmount;
+    }
